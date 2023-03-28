91a75f4e4f7753d85fce85e0684f1f245ba7d129
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-28 02:34:25 -0300

    Added svg icons

--- app/Header/Navbar/Navbar.tsx
+++ app/Header/Navbar/Navbar.tsx
@@ -1,27 +1,42 @@
+import AboutIcon from '@/app/Components/Icons/AboutIcon'
+import CertificatesIcon from '@/app/Components/Icons/CertificatesIcon'
+import ContactIcon from '@/app/Components/Icons/ContactIcon'
+import HomeIcon from '@/app/Components/Icons/HomeIcon'
+import PortfolioIcon from '@/app/Components/Icons/PortfolioIcon'
+
 const iconsnav = [
   {
-    name: 'home'
+    name: 'home',
+    svg: <HomeIcon />
   },
   {
-    name: 'about'
+    name: 'about',
+    svg: <AboutIcon />
   },
   {
-    name: 'portfolio'
+    name: 'portfolio',
+    svg: <PortfolioIcon />
   },
   {
-    name: 'certification'
+    name: 'certificates',
+    svg: <CertificatesIcon />
   },
   {
-    name: 'contact'
+    name: 'contact',
+    svg: <ContactIcon />
   }
 ]
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className="fixed w-full">
+      <ul className="flex gap-8 justify-center p-4">
         {iconsnav.map((icon) => {
-          return <li key={icon.name}>{icon.name}</li>
+          return (
+            <li key={icon.name} className="rounded-full p-2 bg-gray-300">
+              {icon.svg}
+            </li>
+          )
         })}
       </ul>
     </nav>
