ca9124ea4785c52557b22c36bc10aa133a17dab4
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-29 01:21:49 -0300

    added functionality menu

--- app/Header/Navbar/Navbar.tsx
+++ app/Header/Navbar/Navbar.tsx
@@ -1,42 +1,42 @@
+'use client'
 import AboutIcon from '@/app/Components/Icons/AboutIcon'
 import CertificatesIcon from '@/app/Components/Icons/CertificatesIcon'
 import ContactIcon from '@/app/Components/Icons/ContactIcon'
 import HomeIcon from '@/app/Components/Icons/HomeIcon'
 import PortfolioIcon from '@/app/Components/Icons/PortfolioIcon'
+import NavbarIcons from './NavbarIcons'
+import { useNavIcon } from './Hooks/useNavIcon'
 
 const iconsnav = [
   {
     name: 'home',
-    svg: <HomeIcon />
+    svg: HomeIcon
   },
   {
     name: 'about',
-    svg: <AboutIcon />
+    svg: AboutIcon
   },
   {
     name: 'portfolio',
-    svg: <PortfolioIcon />
+    svg: PortfolioIcon
   },
   {
     name: 'certificates',
-    svg: <CertificatesIcon />
+    svg: CertificatesIcon
   },
   {
     name: 'contact',
-    svg: <ContactIcon />
+    svg: ContactIcon
   }
 ]
 
 const Navbar = () => {
+  const { handleIcon, selectedIcon } = useNavIcon()
   return (
     <nav className="fixed w-full">
       <ul className="flex gap-8 justify-center p-4">
-        {iconsnav.map((icon) => {
-          return (
-            <li key={icon.name} className="rounded-full p-2 bg-gray-300">
-              {icon.svg}
-            </li>
-          )
+        {iconsnav.map(({ name, svg }, index) => {
+          return <NavbarIcons key={'navbar-' + name} name={name} Svg={svg} index={index} active={name === selectedIcon} handleIcon={handleIcon} />
         })}
       </ul>
     </nav>
