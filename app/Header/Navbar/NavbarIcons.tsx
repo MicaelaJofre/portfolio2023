0d5b2fbacb92f901b283fe33458c5ac503ac8e22
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-29 01:22:29 -0300

    Created component for icon

---
+++ app/Header/Navbar/NavbarIcons.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from '@/app/Components/Icons/IconsTypes'
+
+type Props = {
+  name: string
+  Svg: ({ color, width, height }: IconProps) => JSX.Element
+  index: number
+  handleIcon: ({ index, name }: { index: number; name: string }) => void
+  active: boolean
+}
+
+const NavbarIcons = ({ name, Svg, index, handleIcon, active }: Props) => {
+  const handleClick = () => {
+    handleIcon({ name, index })
+  }
+  return (
+    <li className={active ? 'rounded-full p-2 bg-yellow-select shadow-xl' : 'rounded-full p-2 bg-white-bg shadow-xl'} onClick={handleClick}>
+      <Svg width={17} height={17} color={'#918e8e'} />
+    </li>
+  )
+}
+
+export default NavbarIcons
