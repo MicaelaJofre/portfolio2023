dd48863e9fee7685daee7f0ac6f21d2e30df4d0d
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-29 01:16:52 -0300

    Added colors and height globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'yellow-select': '#f9dc5c',
+        'white-bg': '#f7f7f7',
+        'gray-icon': '#918e8e'
+      },
+      height: {
+        'section-layout': '30em'
+      }
+    }
   },
   plugins: []
 }
