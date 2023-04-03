6d0be66a27cb6aa49a59a9e2d4dc4ff8418d9258
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-02 23:55:59 -0300

    Config grid and background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,18 @@ module.exports = {
     extend: {
       colors: {
         'yellow-select': '#f9dc5c',
-        'white-bg': '#f7f7f7',
+        'white-bg': '#f4f4f4',
         'gray-icon': '#918e8e'
       },
       height: {
         'section-layout': '30em'
       },
       gridTemplateColumns: {
-        'about-col': 'repeat(auto-fill, minmax(40px, 1fr))'
+        'about-col': 'repeat(auto-fill, minmax(40px, 1fr))',
+        'portfolio-col': 'repeat(auto-fill, minmax(200px, 1fr))'
+      },
+      backgroundImage: {
+        'body-texture': "url('/image/background/bg.jpg')"
       }
     }
   },
