e7f1f583fffa36c01a083c8c82033fd786fc52d5
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-03 00:00:03 -0300

    Change size and content

--- app/Components/Home/Hero.tsx
+++ app/Components/Home/Hero.tsx
@@ -1,21 +1,24 @@
 import Image from 'next/image'
-import perfil from '../../../public/image/avatar1.png'
+import perfil from '../../../public/image/perfil.jpg'
+import AboutIcon from '../Icons/IconsAbout/CssIcon'
 
 const Hero = () => {
   return (
-    <section className="w-11/12 h-section-layout shadow-lg flex">
-      <div className="flex flex-col pl-28 justify-center w-full">
-        <h1>
+    <section className="w-11/12 h-section-layout shadow-lg flex bg-white-bg">
+      <div className="flex flex-col pl-28 justify-center w-7/12">
+        <h1 className="text-4xl">
           <span className="largerPrint">M</span>icaela Jofre
         </h1>
-        <p className="font-bold text-black opacity-70 text-xl">Soy Desarrolladora Fullstack</p>
-        <p className="text-xs font-light p-2 pl-0">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since.
-        </p>
+        <p className="font-bold text-gray-400 text-sm">Developer Fullstack</p>
+        <div className="flex gap-10 pt-4">
+          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
+          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
+          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
+          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
+        </div>
       </div>
-      <div className="w-full flex justify-center items-center">
-        <Image width={220} height={220} className="" src={perfil} alt="" />
+      <div className="w-3/12 flex justify-center items-center">
+        <Image width={220} height={220} className="rounded-full border-4 border-gray-600" src={perfil} alt="avatar" />
       </div>
     </section>
   )
