39b1e63a25b7c4e4aa9ab3a99487926de9032fe0
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-02 23:57:34 -0300

    implements component Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from './Components/About/About'
 import Hero from './Components/Home/Hero'
+import Portfolio from './Components/Portfolio/Portfolio'
 
 export default function Home() {
   return (
     <main className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-20 pt-16">
       <Hero />
       <About />
+      <Portfolio />
     </main>
   )
 }
