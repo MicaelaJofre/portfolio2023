c08c8de87dc6c848b1801e129e786b6e7aaae266
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-12 00:00:58 -0300

    Reformed the structure of the hero

--- app/Components/Home/Hero.tsx
+++ app/Components/Home/Hero.tsx
@@ -1,24 +1,26 @@
-import Image from 'next/image'
-import perfil from '../../../public/image/perfil.jpg'
-import AboutIcon from '../Icons/IconsAbout/CssIcon'
+import Arrow from '../Icons/IconSocial/Arrow'
+import Email from '../Icons/IconSocial/Email'
+import GitHub from '../Icons/IconSocial/GitHub'
+import Linkedin from '../Icons/IconSocial/Linkedin'
+import WhatsApp from '../Icons/IconSocial/WhatsApp'
 
 const Hero = () => {
   return (
-    <section className="w-11/12 h-section-layout shadow-lg flex bg-white-bg">
-      <div className="flex flex-col pl-28 justify-center w-7/12">
-        <h1 className="text-4xl">
-          <span className="largerPrint">M</span>icaela Jofre
+    <section className="w-11/12 lg:w-10/12 mt-5 h-section-layout shadow-lg flex bg-yellow-500">
+      <div className="flex flex-col pl-20 justify-center w-full">
+        <h1 className="title">
+          <span className="text-gray-100 text-8xl underline decoration-solid">M</span>icaela Jofre
         </h1>
-        <p className="font-bold text-gray-400 text-sm">Developer Fullstack</p>
-        <div className="flex gap-10 pt-4">
-          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
-          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
-          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
-          <AboutIcon width={30} height={30} color={'#A6A6A6'} />
+        <p className='text-xs pt-4 tracking-wide leading-4 font-bold'>¡Gracias por visitar mi portafolio! Aquí encontrarás una selección de mis proyectos más recientes y una breve descripción de mi experiencia y habilidades. Abajo podrás ver un poco más acerca de mi historia y cómo he llegado a donde estoy hoy. ¡Bienvenidos!</p>
+        <div className="flex gap-10 pt-5">
+          <a href='#'><GitHub width={30} height={30} color={'#181616'} /></a>
+          <a href='#'><Linkedin width={30} height={30} color={'#181616'} /></a>
+          <a href='#'><WhatsApp width={30} height={30} color={'#181616'} /></a>
+          <a href='#'><Email width={30} height={30} color={'#181616'} /></a>
         </div>
       </div>
-      <div className="w-3/12 flex justify-center items-center">
-        <Image width={220} height={220} className="rounded-full border-4 border-gray-600" src={perfil} alt="avatar" />
+      <div className='w-2/4 flex justify-end items-end p-8'>
+        <Arrow width={50} height={50} color={'#181616'}/>
       </div>
     </section>
   )
