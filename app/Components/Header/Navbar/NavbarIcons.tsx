665c288ca85c8cd6d1ef970f124f1150b4e009c0
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-11 23:59:43 -0300

    Change color

--- app/Components/Header/Navbar/NavbarIcons.tsx
+++ app/Components/Header/Navbar/NavbarIcons.tsx
@@ -13,8 +13,8 @@ const NavbarIcons = ({ name, Svg, index, handleIcon, active }: Props) => {
     handleIcon({ name, index })
   }
   return (
-    <li className={active ? 'rounded-full p-2 bg-yellow-select shadow-xl' : 'rounded-full p-2 bg-white-bg shadow-xl'} onClick={handleClick}>
-      <Svg width={17} height={17} color={'#918e8e'} />
+    <li className={active ? 'rounded-full p-2 bg-yellow-500 shadow-xl' : 'rounded-full p-2 bg-white-bg shadow-xl'} onClick={handleClick}>
+      <Svg width={17} height={17} color={'#181616'} />
     </li>
   )
 }
