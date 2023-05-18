19eaf5ecf4160081a270783826b7e46b8904f6cc
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-18 01:43:03 -0300

    Created hook from filter

---
+++ app/Components/Portfolio/useFilter.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+import Proyects from './Proyects.json'
+
+type UserData = {
+  filter?: string
+}
+
+export const useFilter = ({ filter }: UserData) => {
+  const listProyect = Proyects.slice(0, 6)
+  const [proyects, setproyects] = useState(listProyect)
+  const newProyects = useRef(listProyect)
+
+  const getproyects = () => {
+    switch (filter) {
+      case 'Últimos proyectos':
+        setproyects(listProyect)
+        break
+
+      case 'React / Next':
+        newProyects.current = Proyects.filter((proyect) => proyect.category === 'react/next')
+        setproyects(newProyects.current)
+        break
+
+      case 'Apis':
+        newProyects.current = Proyects.filter((proyect) => proyect.category === 'api')
+        setproyects(newProyects.current)
+        break
+
+      case 'Random':
+        newProyects.current = Proyects.filter((proyect) => proyect.category === 'random')
+        setproyects(newProyects.current)
+        break
+    }
+  }
+
+  useEffect(() => {
+    getproyects()
+  }, [filter])
+
+  console.log(proyects)
+  return {
+    proyects,
+    getproyects
+  }
+}
