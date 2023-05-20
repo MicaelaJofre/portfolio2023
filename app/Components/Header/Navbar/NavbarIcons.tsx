f44642d397e45ba92844eb8f874c1ab039b94caa
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-19 23:34:26 -0300

    Added hover nav

--- app/Components/Header/Navbar/NavbarIcons.tsx
+++ app/Components/Header/Navbar/NavbarIcons.tsx
@@ -13,7 +13,7 @@ const NavbarIcons = ({ name, Svg, index, handleIcon, active }: Props) => {
     handleIcon({ name, index })
   }
   return (
-    <li className={active ? 'rounded-full p-2 bg-yellow-500 shadow-xl' : 'rounded-full p-2 bg-white-bg shadow-xl'} onClick={handleClick}>
+    <li className={active ? 'rounded-full p-2 bg-yellow-500 shadow-xl arrow' : 'rounded-full p-2 bg-white-bg shadow-xl arrow'} onClick={handleClick}>
       <Svg width={17} height={17} color={'#181616'} />
     </li>
   )
