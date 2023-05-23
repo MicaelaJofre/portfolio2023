29b3cf8599da98440a7e857fac70ba0dc3f3a0af
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-23 00:33:25 -0300

    Change class and structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,19 @@ import Portfolio from './Components/Portfolio/Portfolio'
 
 export default function Home() {
   return (
-    <main className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-20 pt-16">
-      <Hero />
-      <About />
-      <Portfolio />
-      <Contact/>
+    <main id="home" title="home" className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-20 pt-16">
+      <div className='w-full  flex justify-center items-center'>
+        <Hero />
+      </div>
+      <div id="about" title="about" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
+        <About />
+      </div>
+      <div id="portfolio" title="portfolio" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
+        <Portfolio />
+      </div>
+      <div id="contact" title="contact" className='pt-11 w-full 2x1:w-9/12 flex justify-center items-center'>
+        <Contact/>
+      </div>
     </main>
   )
 }
