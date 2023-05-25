dae782e31acb28231bac10e0773903278c973be0
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-24 23:46:12 -0300

    Delete title

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ import Portfolio from './Components/Portfolio/Portfolio'
 
 export default function Home() {
   return (
-    <main id="home" title="home" className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-20 pt-16">
-      <div className='w-full  flex justify-center items-center'>
+    <main id="home" className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-20 pt-16">
+      <div className='w-full flex justify-center items-center'>
         <Hero />
       </div>
-      <div id="about" title="about" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
-        <About />
+      <div id="about" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
+        <About/>
       </div>
-      <div id="portfolio" title="portfolio" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
+      <div id="portfolio" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
         <Portfolio />
       </div>
-      <div id="certificates" title="certificates" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
+      <div id="certificates" className='pt-14 w-full 2x1:w-9/12 flex justify-center items-center'>
         <Certificates/>
       </div>
-      <div id="contact" title="contact" className='pt-11 w-full 2x1:w-9/12 flex justify-center items-center'>
+      <div id="contact" className='pt-11 w-full 2x1:w-9/12 flex justify-center items-center'>
         <Contact/>
       </div>
     </main>
