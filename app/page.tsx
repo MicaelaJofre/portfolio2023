a61085328a2cbab4f11ed3e88def9f31261cea40
Micaela Analia Jofre <mkjofre@gmail.com>
2023-06-04 22:36:07 -0300

    fixed min-h

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,27 @@ export default function Home() {
   return (
     <main className="w-full h-auto pb-16 flex flex-col justify-center items-center gap-40">
       <Waypoints target={'tohome'}>
-        <div id="home" className='w-screen min-h-screen flex justify-center items-center pt-10'>
+        <div id="home" className='w-screen  flex justify-center items-center pt-20'>
           <Hero />
         </div>
       </Waypoints>
       <Waypoints target={'toabout'}>
-        <div id="about" className='w-screen min-h-screen flex justify-center items-center pt-10 mt-20'>
+        <div id="about" className='w-screen min-h-screen flex justify-center items-center pt-20'>
           <About/>
         </div>
       </Waypoints>
       <Waypoints target={'toportfolio'}>
-        <div id="portfolio" className='w-screen min-h-screen flex justify-center items-center pt-10 mt-20'>
+        <div id="portfolio" className='w-screen min-h-screen flex justify-center items-center pt-16'>
           <Portfolio />
         </div>
       </Waypoints>
       <Waypoints target={'tocertificates'}>
-        <div id="certificates" className='w-screen min-h-screen flex justify-center items-center pt-10 mt-20'>
+        <div id="certificates" className='w-screen min-h-screen flex justify-center items-center pt-16'>
           <Certificates/>
         </div>
       </Waypoints>
       <Waypoints target={'tocontact'}>
-        <div id="contact" className='w-screen min-h-screen flex justify-center items-center mt-20'>
+        <div id="contact" className='w-screen min-h-screen flex justify-center items-center pt-16'>
           <Contact/>
         </div>
       </Waypoints>
